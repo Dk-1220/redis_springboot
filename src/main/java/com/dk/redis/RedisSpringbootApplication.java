12bc9773cf625d34a11422da692c3d3c81cc4fdf
D <1285434409@qq.com>
2022-04-20 22:36:43 +0800

    Spring整合Redis项目

---
+++ src/main/java/com/dk/redis/RedisSpringbootApplication.java
@@ -0,0 +1,11 @@
+package com.dk.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSpringbootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisSpringbootApplication.class);
+    }
+}
